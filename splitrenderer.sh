1de8706621f6a7e5b389cdf37993e1109e7db9a3
PabloVD <pablo.villanueva.domingo@gmail.com>
2024-11-25 13:29:47 +0100

    Move old files to gitignore

--- splitrenderer.sh
+++ splitrenderer.sh
@@ -6,12 +6,12 @@ python3 splitrenderer.py \
     --out /home/tda/CARLA/TrafficGeneration/Datasets/Waymo_tf_example/tests_prerendered \
     #--use-vectorize \
 
-python3 splitrenderer.py \
-    --data "/media/tda/Crucial X6/PabloComputer/TrafficGenerationDatasets/Waymo_tf_example_train" \
-    --out /home/tda/CARLA/TrafficGeneration/Datasets/Waymo_tf_example/rendered_train_fixed
+#python3 splitrenderer.py \
+#    --data "/media/tda/Crucial X6/PabloComputer/TrafficGenerationDatasets/Waymo_tf_example_train" \
+#    --out /home/tda/CARLA/TrafficGeneration/Datasets/Waymo_tf_example/rendered_train_fixed
     
-python3 splitrenderer.py \
-    --data "/media/tda/Crucial X6/PabloComputer/TrafficGenerationDatasets/Waymo_tf_example_valid" \
-    --out /home/tda/CARLA/TrafficGeneration/Datasets/Waymo_tf_example/rendered_valid_fixed \
+#python3 splitrenderer.py \
+#    --data "/media/tda/Crucial X6/PabloComputer/TrafficGenerationDatasets/Waymo_tf_example_valid" \
+#    --out /home/tda/CARLA/TrafficGeneration/Datasets/Waymo_tf_example/rendered_valid_fixed \
 #    --use-vectorize
 
